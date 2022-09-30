0ba4f73029e37f7f953d669098304b644810b585
maksonchik20 <elena-g0903@mail.ru>
2022-09-30 20:51:18 +0300

    Решил задачу 5kyu

---
+++ 5kyu/kata_5259510fc76e59579e0009d4.py
@@ -0,0 +1,16 @@
+import difflib
+class Dictionary:
+    def __init__(self,words):
+        self.words=words
+    def find_most_similar(self,term):
+        collections = {}
+        for word in self.words:
+            ret = difflib.SequenceMatcher(None, word, term).ratio()
+            collections[word] = ret
+        print(collections)
+        return sorted(collections.items(), key=lambda x: x[1])[-1][0]
+
+words = ['codewars', 'code', 'stars', 'wars', 'codec', 'mars']
+a = Dictionary(words)
+
+print(a.find_most_similar('coddwars'))
