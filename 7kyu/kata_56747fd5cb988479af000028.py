0fc5ea7d840ab1e8dddda5ba9df3fd61fd43f537
maksonchik20 <elena-g0903@mail.ru>
2022-09-30 19:24:17 +0300

    Решил задачу 7kyu

---
+++ 7kyu/kata_56747fd5cb988479af000028.py
@@ -0,0 +1,10 @@
+# Get the Middle Character
+
+# Kata.getMiddle("test") should return "es"
+# Kata.getMiddle("testing") should return "t"
+# Kata.getMiddle("middle") should return "dd"
+# Kata.getMiddle("A") should return "A"
+
+def get_middle(s):
+    return s[(len(s)//2)-1:((len(s)//2) + 1)] if len(s) % 2 ==0 else s[(len(s)//2)]
+print(get_middle(input()))
